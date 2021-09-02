94d162fc962d0cb98a17f83e13586dddbdcde4e7
ivanovsemyon <sivanov.exceedteam@gmail.com>
2021-09-02 12:41:18 +0300

    error messege

--- modules/controllers/item.controller.js
+++ modules/controllers/item.controller.js
@@ -17,35 +17,43 @@ connect(
 const User = model("users", userSchema);
 
 module.exports.register = async (req, res) => {
-  const { login, password } = req.body;
-  const passwordRegex = /^(?=.*[\d])[A-Za-z0-9].{5,}$/;
+  const { login, password, repeatPassword } = req.body;
+  if (login && password && repeatPassword) {
+    const passwordRegex = /^(?=.*[\d])[A-Za-z0-9].{5,}$/;
 
-  const validLogin = login.length >= 6;
-  const validPassword = passwordRegex.test(password);
-  const candidate = await User.findOne({ login });
+    const validLogin = login.length < 6;
+    const validPassword = passwordRegex.test(password);
+    const candidate = await User.findOne({ login });
 
-  if (!validLogin) {
-    return res.status(400).send("Длина логина должна быть не менее 6 символов");
+    if (validLogin) {
+      return res
+        .status(400)
+        .send({ login: "Длина логина должна быть не менее 6 символов" });
+    }
+    if (!validPassword) {
+      return res.status(400).send({
+        password:
+          "Длина пароля должна быть не меньше 6 символов, обязательно состоять из латинских символов и содержать число",
+      });
+    }
+    if (password !== repeatPassword) {
+      return res.status(400).send({
+        repeatPassword: "Пароли не совпадают",
+      });
+    }
+
+    if (candidate) {
+      return res.status(400).send({ login: "Такой логин уже существует" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 4);
+
+    await User.create({
+      login: login,
+      password: hashedPassword,
+    });
+    res.send("Пользователь создан");
+  } else {
+    res.status(400).send("Введите корректные данные");
   }
-  if (!validPassword) {
-    return res
-      .status(400)
-      .send(
-        "Длина пароля должна быть не меньше 6 символов, обязательно состоять из латинских символов и содержать число"
-      );
-  }
-
-  if (candidate) {
-    return res
-      .status(400)
-      .send({ error: { message: "Такой логин уже существует" } });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 12);
-
-  await User.create({
-    login: login,
-    password: hashedPassword,
-  });
-  res.send("Пользователь создан");
 };
